55fb59a765936ec72a5c923abeed36c3ee6451fb
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-04-18 16:06:38 +0530

    Time: 2 ms (58.56%), Space: 5.8 MB (96.49%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,20 +3,9 @@ public:
     int hammingWeight(uint32_t N) {
         
         int count0 = 0, count1 = 0;
- 
-    // Iterate through all the bits
     while (N > 0) {
- 
-        // If current bit is 1
-        if (N & 1) {
-            count1++;
-        }
- 
-        // If current bit is 0
-        else {
-            count0++;
-        }
- 
+        if (N & 1) count1++;
+        else count0++;
         N = N >> 1;
         
     }
