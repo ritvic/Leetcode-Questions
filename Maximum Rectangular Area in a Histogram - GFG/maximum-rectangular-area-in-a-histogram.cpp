979c0e8e48aa58624d2cdf7ee50f76fb78f98ebd
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-05-19 20:25:56 +0530

    Added solution - LeetHub

---
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long hist[], int n)
+    {
+       stack<int> s;
+ 
+    long long max_area = 0;
+    long long tp; 
+    long long area_with_top;
+     int i = 0;
+    while (i < n)
+    {
+        if (s.empty() || hist[s.top()] <= hist[i])
+            s.push(i++);
+        else
+        {
+            tp = s.top();
+            s.pop(); 
+
+            area_with_top = hist[tp] * (s.empty() ? i :
+                                   i - s.top() - 1);
+ 
+            if (max_area < area_with_top)
+                max_area = area_with_top;
+        }
+    }
+
+    while (s.empty() == false)
+    {
+        tp = s.top();
+        s.pop();
+        area_with_top = hist[tp] * (s.empty() ? i :
+                                i - s.top() - 1);
+ 
+        if (max_area < area_with_top)
+            max_area = area_with_top;
+    }
+ 
+    return max_area;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.getMaxArea(arr, n)<<endl;
+    
+    }
+	return 0;
+}
+  // } Driver Code Ends
