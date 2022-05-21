7585743e425011202d5b5253a1f79d3b42b8bef9
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-05-21 12:42:25 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -8,43 +8,42 @@ class Solution
 {
     public:
     //Function to find largest rectangular area possible in a given histogram.
-    long long getMaxArea(long long hist[], int n)
+    long long getMaxArea(long long v[], int n)
     {
-       stack<int> s;
- 
-    long long max_area = 0;
-    long long tp; 
-    long long area_with_top;
-     int i = 0;
-    while (i < n)
-    {
-        if (s.empty() || hist[s.top()] <= hist[i])
-            s.push(i++);
-        else
-        {
-            tp = s.top();
-            s.pop(); 
-
-            area_with_top = hist[tp] * (s.empty() ? i :
-                                   i - s.top() - 1);
- 
-            if (max_area < area_with_top)
-                max_area = area_with_top;
-        }
-    }
-
-    while (s.empty() == false)
-    {
-        tp = s.top();
-        s.pop();
-        area_with_top = hist[tp] * (s.empty() ? i :
-                                i - s.top() - 1);
- 
-        if (max_area < area_with_top)
-            max_area = area_with_top;
-    }
- 
-    return max_area;
+        vector<long long> r1(n,n),r2(n,-1);
+       stack<pair<long long,int>> st,st1;
+       st.push({v[0],0});
+       for(int i=0;i<n;i++)
+       {
+           while(!st.empty() && st.top().first>v[i])
+           {
+               r1[st.top().second]=i;
+               st.pop();
+           }
+           st.push({v[i],i});
+       }
+       st1.push({v[n-1],n-1});
+        for(int i=n-2;i>=0;i--)
+       {
+           while(!st1.empty() && st1.top().first>v[i])
+           {
+               r2[st1.top().second]=i;
+               st1.pop();
+           }
+           st1.push({v[i],i});
+       }
+       long long maxx=-1;
+       for(int i=0;i<n;i++)
+           maxx=max(maxx,(r1[i]-r2[i]-1)*v[i]);
+           
+        // for(auto i:r1)
+        // cout<<i<<" ";
+        // cout<<endl;
+        // for(auto i:r2)
+        // cout<<i<<" ";
+           
+    return maxx;
+       
     }
 };
 
