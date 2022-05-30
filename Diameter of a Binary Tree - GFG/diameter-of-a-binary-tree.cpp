aad05fc243e878ba2fa9b5b40a93a5a05041dcb9
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-05-30 23:14:15 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -93,35 +93,38 @@ struct Node
 
 class Solution {
   public:
-    // Function to return the diameter of a Binary Tree.
-    int diameter(Node* tree) {
-        // Your code here
-        if (tree == NULL)
-        return 0;
- 
-    // get the height of left and right sub-trees
-    int lheight = height(tree->left);
-    int rheight = height(tree->right);
- 
-    // get the diameter of left and right sub-trees
-    int ldiameter = diameter(tree->left);
-    int rdiameter = diameter(tree->right);
- 
-    // Return max of following three
-    // 1) Diameter of left subtree
-    // 2) Diameter of right subtree
-    // 3) Height of left subtree + height of right subtree + 1
-    return max(lheight + rheight + 1, max(ldiameter, rdiameter));
-    } 
-    int height(struct Node* node)
+    // Function to return the diameter of a Binary Tree.z
+    int diameterOpt(Node* root, int* height)
     {
-        // base case tree is empty
-        if (node == NULL)
-            return 0;
-     
-        // If tree is not empty then height = 1 + max of left
-        // height and right heights
-        return 1 + max(height(node->left), height(node->right));
+        // lh --> Height of left subtree
+        // rh --> Height of right subtree
+        int lh = 0, rh = 0;
+      
+        // ldiameter  --> diameter of left subtree
+        // rdiameter  --> Diameter of right subtree
+        int ldiameter = 0, rdiameter = 0;
+      
+        if (root == NULL) {
+            *height = 0;
+            return 0; // diameter is also 0
+        }
+      
+        // Get the heights of left and right subtrees in lh and
+        // rh And store the returned values in ldiameter and
+        // ldiameter
+        ldiameter = diameterOpt(root->left, &lh);
+        rdiameter = diameterOpt(root->right, &rh);
+      
+        // Height of current node is max of heights of left and
+        // right subtrees plus 1
+        *height = max(lh, rh) + 1;
+      
+        return max(lh + rh + 1, max(ldiameter, rdiameter));
+    }
+    int diameter(Node* root) {
+        // Your code here
+        int h=0;
+        return diameterOpt(root,&h);
     }
 };
 
