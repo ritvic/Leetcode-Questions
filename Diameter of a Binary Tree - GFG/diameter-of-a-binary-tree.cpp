95fb81ef04450da2bb211c74f8cbe953a1463802
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-05-31 11:05:57 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -94,37 +94,29 @@ struct Node
 class Solution {
   public:
     // Function to return the diameter of a Binary Tree.z
-    int diameterOpt(Node* root, int* height)
-    {
-        // lh --> Height of left subtree
-        // rh --> Height of right subtree
-        int lh = 0, rh = 0;
-      
-        // ldiameter  --> diameter of left subtree
-        // rdiameter  --> Diameter of right subtree
-        int ldiameter = 0, rdiameter = 0;
-      
-        if (root == NULL) {
-            *height = 0;
-            return 0; // diameter is also 0
-        }
-      
-        // Get the heights of left and right subtrees in lh and
-        // rh And store the returned values in ldiameter and
-        // ldiameter
-        ldiameter = diameterOpt(root->left, &lh);
-        rdiameter = diameterOpt(root->right, &rh);
-      
-        // Height of current node is max of heights of left and
-        // right subtrees plus 1
-        *height = max(lh, rh) + 1;
-      
-        return max(lh + rh + 1, max(ldiameter, rdiameter));
-    }
+    int height(Node* root, int& ans)
+{
+    if (root == NULL)
+        return 0;
+ 
+    int left_height = height(root->left, ans);
+ 
+    int right_height = height(root->right, ans);
+ 
+    // update the answer, because diameter of a
+    // tree is nothing but maximum value of
+    // (left_height + right_height + 1) for each node
+    ans = max(ans, 1 + left_height + right_height);
+ 
+    return 1 + max(left_height, right_height);
+}
     int diameter(Node* root) {
         // Your code here
-        int h=0;
-        return diameterOpt(root,&h);
+        if (root == NULL)
+        return 0;
+    int ans = INT_MIN; // This will store the final answer
+    height(root, ans);
+    return ans;
     }
 };
 
