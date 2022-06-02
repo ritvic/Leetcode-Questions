e562200eff4c7f0d3915dce7c38986425d3088bd
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-02 21:56:30 +0530

    Added solution - LeetHub

--- Count of strings that can be formed using a, b and c under given constraints - GFG/count-of-strings-that-can-be-formed-using-a-b-and-c-under-given-constraints.cpp
+++ Count of strings that can be formed using a, b and c under given constraints - GFG/count-of-strings-that-can-be-formed-using-a-b-and-c-under-given-constraints.cpp
@@ -15,8 +15,35 @@ return 0;
 }// } Driver Code Ends
 
 
+
 long long int countStr(long long int n){
     //complete the function here
-        return n*(n*n+1)/2 + n + 1;
+         // 1length -> 1-b, 1-c --> 2
+   // 2length -> 1-b:1-c, 2-c --> 2+1
+   // 3length -> 1-b:2-c; --> 3
+   
+   int onelength = 2;
+   int twolength = 3;
+   int threelength = 3;
+   
+   if(n == 1){
+       return onelength+1;
+   }
+   
+   long long int count = 1;
+   if(n == 2){
+       return count+4+2+1;
+   }
+   
+   // For 1 case
+   count += n*2;
+   
+   // For 2 case;
+   count += n*(n-1) + (n*(n-1))/2;
+   
+   // For 3 case;
+   count += (n*(n-1)*(n-2))/2;
+   
+   return count;
 
 }
