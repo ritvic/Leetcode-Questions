359b317fa9a5279bb1e08b42463f58d94d5f8d59
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-02 11:55:14 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -4,26 +4,53 @@ using namespace std;
 
  // } Driver Code Ends
 
-// vector<int> dp(100001,-1);
+vector<int> dp(100001,1);
 class Solution
 {
     public:
+    int fun(int n,int a[])
+    {
+        int ans=1;
+        if(dp[n]!=-1)
+        return dp[n];
+        dp[n]=1;
+        for(int i=0;i<n;i++)
+        {
+            if(a[n]>a[i])
+            dp[n]=max(dp[n],1+fun(i,a));
+
+        }
+        return dp[n];
+    }
+
     int longestSubsequence(int n, int a[])
     {
        // your code here
-       vector<int> dp(n,1);
-       int ans=0;
-       for(int i=1;i<n;i++)
-       {
-           for(int j=0;j<i;j++)
-           {
-               if(a[i]>a[j])
-               dp[i]=max(dp[i],1+dp[j]);
-           }
-           ans=max(ans,dp[i]);
-       }
-       return ans;
+       int maxx=0;
+       dp=vector<int> (100001,-1);
+       for(int i=0;i<n;i++)
+       maxx=max(maxx,fun(i,a));
+
+       return maxx;
+    //     for(int i=0;i<n;i++)
+    //   cout<<dp[i]<<" ";
     }
+    // int longestSubsequence(int n, int a[])
+    // {
+    //   // your code here
+    //   vector<int> dp(n,1);
+    //   int ans=0;
+    //   for(int i=1;i<n;i++)
+    //   {
+    //       for(int j=0;j<i;j++)
+    //       {
+    //           if(a[i]>a[j])
+    //           dp[i]=max(dp[i],1+dp[j]);
+    //       }
+    //       ans=max(ans,dp[i]);
+    //   }
+    //   return ans;
+    // }
 };
 
 // { Driver Code Starts.
