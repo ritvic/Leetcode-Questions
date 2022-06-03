294d11133838fbd011f56c90c8f92cc13be5258a
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-03 12:17:42 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,36 +6,29 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-   bool iscycle(int x,vector<int> &vis,vector<int> adj[],vector<int> &stack)
-    {
-        stack[x]=1;
-        if(!vis[x])
-        {
-            vis[x]=1;
-        
-            for(auto i:adj[x])
-            {
-                if(!vis[i] && iscycle(i,vis,adj,stack))
-                    return true;
-                if(stack[i])
-                    return true;
-            }
-                
-        }
-        stack[x]=0;
-        return false;
-    }
-    bool isCyclic(int V, vector<int> adj[]) {
-        // Code here
-        vector<int> stack(V+1,0);
-        vector<int> vis(V+1,0);
-        bool cycle=false;
-        for(int i=0;i<V;i++)
-        {
-            if(!vis[i] && iscycle(i,vis,adj,stack))
-            cycle=true;
-        }
-        return cycle;
+   bool isCyclic(int V, vector<int> adj[]) {
+        vector <int> indegree(V, 0);
+	    for(int i=0; i<V; i++){
+	        for(auto j:adj[i])
+	            indegree[j]++;
+	    }
+	    queue <int> q;
+	    for(int i=0; i<V; i++){
+	        if(indegree[i] == 0)
+	            q.push(i);
+	    }
+	    int c = 0;
+	    while(!q.empty()){
+	        int t = q.front();
+	        q.pop();
+	        c += 1;
+	        for(auto i:adj[t]){
+	            indegree[i]--;
+	            if(indegree[i] == 0)
+	                q.push(i);
+	        }
+	    }
+	    return c == V ? 0 : 1;
     }
 };
 
