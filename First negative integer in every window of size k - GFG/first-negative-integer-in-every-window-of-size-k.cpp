4943173c1253ffe8eade9f5a8ca22f50ded11b64
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-04 22:27:50 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int a[],
+                                             long long int n, long long int k) {
+                                                 
+         vector<long long int>v;
+    vector<long long int>ls;
+    long long int i=0,j=0;
+    while(j<n)
+    {
+        if(a[j]<0)
+        {
+            ls.push_back(a[j]);
+        }
+        if(j-i+1<k)
+        {
+            j++;
+        }
+        else if(j-i+1==k)
+        {
+           if(a[i]>=0)
+           {
+               if(ls.size()==0)
+            {
+                v.push_back(0);
+            }
+            else 
+            {
+               v.push_back(ls.front());
+            }
+           }
+           else if(a[i]<0)
+           {
+               if(ls.size()==0)
+            {
+                v.push_back(0);
+            }
+            else 
+            {
+               v.push_back(ls.front());
+               ls.erase(ls.begin());
+            }
+           }
+            i++;
+            j++;
+        }
+    }
+    return v;                               
+                                                 
+ }
