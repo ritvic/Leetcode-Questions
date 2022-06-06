d93efb9fe88786b3473aaf9bcaf87b394c25298a
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-06 21:55:08 +0530

    Added solution - LeetHub

--- Count More than n/k Occurences - GFG/count-more-than-nk-occurences.cpp
+++ Count More than n/k Occurences - GFG/count-more-than-nk-occurences.cpp
@@ -14,7 +14,7 @@ class Solution
     int countOccurence(int arr[], int n, int k) {
         // Your code here
         
-        map<int ,int >mp;
+        unordered_map<int ,int >mp;
         int count=0;
         for(int i=0;i<n;i++)
         {
