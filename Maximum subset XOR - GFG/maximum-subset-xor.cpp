1a9650c7383173eea945ea9153defcd4263a7b41
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-09 23:31:05 +0530

    Added solution - LeetHub

---
+++ Maximum subset XOR - GFG/maximum-subset-xor.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// Function to return maximum XOR subset in set[]
+
+class Solution
+{
+    public:
+    int maxSubarrayXOR(int arr[], int N)
+    {
+        //Your code here
+        if(N == 0) return 0;
+       int num = 0;
+       while(true)
+       {
+         int maxx = INT_MIN;
+         for(int i=0;i<N;i++){
+             if(maxx<arr[i]) maxx=arr[i];
+         }
+         if(maxx == 0) return num;
+         num = max(num,num^maxx);
+         for(int i=0;i<N;i++)
+         {
+           arr[i] = min(arr[i],arr[i]^maxx);
+         }
+       }
+   }
+};
+
+// { Driver Code Starts.
+int main()
+{
+
+    int t,n,a[100004],k;
+    scanf("%d",&t);
+    while(t--)
+    {
+        //cin>>n;
+       scanf("%d",&n);
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        Solution obj;
+        printf("%d\n",obj.maxSubarrayXOR(a,n));
+       // cout<<bin(a,0,n-1,k)<<endl;
+    }
+}  // } Driver Code Ends
