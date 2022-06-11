227d05efabfeaa7f44c5322e40a2d2ecf767df05
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-11 16:58:50 +0530

    Time: 30 ms (54.34%), Space: 16.6 MB (81.06%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int jump(vector<int>& arr) {
-
-        int n=arr.size();
-        int farthest=0,jumps=0,current=0;
+    int jump(vector<int>& nums) {
+        
+        int farthest=0,current=0,jump=0,n=nums.size();
         for(int i=0;i<n-1;i++)
         {
-          farthest=max(farthest,i+arr[i]);
-          if(i==current)
-          {
-              jumps++;
-              current=farthest;
-          }
-               
+            farthest=max(farthest,i+nums[i]);
+            if(i==current){jump++;current=farthest;}
         }
-        if(current<n-1)
-        return -1;
-        return jumps;
-
+        if(current<n-1) return -1;
+        return jump;
         
     }
 };
