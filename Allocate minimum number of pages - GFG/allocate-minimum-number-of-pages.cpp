a0b14f87b48a2205316dc12fbef912c51875be00
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-14 13:39:16 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -8,50 +8,54 @@ using namespace std;
  // } Driver Code Ends
 //User function template in C++
 
-class Solution 
+class Solution
 {
-    public:
-    //Function to find minimum number of pages.
-    int canBe(int arr[],int n,int m,int mid){
-   int student=1;
-   int page=0;
-   for(int i=0;i<n;i++){
-       if(page+arr[i]<=mid){
-           page+=arr[i];
-       }else{
-           student++;
-           if(student>m || arr[i]>mid){
-               return false;
-           }
-           page=arr[i];
-       }
-   }
-   return true;
-}
-   int findPages(int A[], int N, int M) 
-   {
-       //code here
-       int start=0;
-       int sum=0;
-       int ans=-1;
-       for(int i=0;i<N;i++){
-           sum+=A[i];
-       }
-       int end=sum;
-       
-       while(start<=end){
-           int mid=start+((end-start)/2);
-           if(canBe(A,N,M,mid)){
-               ans=mid;
-               end=mid-1;
-           }else{
-               start=mid+1;
-           }
-       }
-       return ans;
-       
-       
-   }
+public:
+    // Function to find minimum number of pages.
+    int canBe(int arr[], int n, int m, int mid)
+    {
+        int student = 1;
+        int page = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > mid)
+                return false;
+            if(page+arr[i]>mid)
+            {
+                student++;page=arr[i];
+                if(student>m) return false;
+            }
+            else page+=arr[i];
+        }
+        return true;
+    }
+    int findPages(int A[], int N, int M)
+    {
+        // code here
+        int start = 0;
+        int sum = 0;
+        int ans = -1;
+        for (int i = 0; i < N; i++)
+        {
+            sum += A[i];
+        }
+        int end = sum;
+
+        while (start <= end)
+        {
+            int mid = start + ((end - start) / 2);
+            if (canBe(A, N, M, mid))
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
 };
 
 // { Driver Code Starts.
