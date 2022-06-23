504016668bb82d84424beabf931ae89525b3af3f
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-23 20:18:22 +0530

    Added solution - LeetHub

--- Villain Con - GFG/villain-con.cpp
+++ Villain Con - GFG/villain-con.cpp
@@ -9,28 +9,40 @@ using namespace std;
 
 class Solution{
 public:
-    int dfs(int x,vector<int>adj[],int t[]){
-        if(t[x])
-            return t[x];
-        t[x] = 0;
-        for(auto it:adj[x]){
-                t[x] = max(t[x],dfs(it,adj,t));
-        }
-        t[x]+=1;
-        return t[x];
-    }
     int minColour(int N, int M, vector<int> mat[]) {
-        vector<int>adj[N];
-        for(int i=0;i<M;i++){
-            adj[mat[i][1]-1].push_back(mat[i][0]-1);
+
+        vector<vector<int>> ad(N+1);
+        vector<int> in(N+1,0);
+        for(int i = 0; i < M;i++){
+            ad[mat[i][1]].push_back(mat[i][0]);            
+            in[mat[i][0]]++;
+
         }
-        int ans=-1;
-        int t[N]={0};
-        for(int i=0;i<N;i++){
-            ans=max(ans,dfs(i,adj,t));
+        queue<int> helper;
+        for(int i = 1; i<=N;i++){
+            if(in[i]==0){
+                helper.push(i);
+            }
+        }
+        int ans = 0;
+        while(helper.size()!=0){
+            int k = helper.size();
+            for(int i = 0; i < k;i++){
+                int node = helper.front();
+                helper.pop();
+                for(int j = 0; j < ad[node].size();j++){
+                    if(in[ad[node][j]]>=1){
+                        in[ad[node][j]]--;
+                        if(in[ad[node][j]]==0){
+                            helper.push(ad[node][j]);
+                        }
+                    }
+                }
+            }
+            ans++;
         }
         return ans;
-    }
+    } 
 };
 
 // { Driver Code Starts.
