b849039fe6686209bef81d4176665a4edc0550dd
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-24 18:02:23 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -6,26 +6,69 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-        
-    // A[]: input array
-    // N: size of array
-    // Function to find the maximum index difference.
-    int maxIndexDiff(int A[], int N) 
+
+    // int maxIndexDiff(int A[], int N) 
+    // { 
+    //     // Your code here
+    //     int l=0,r=N-1,maxx=-1;
+    //   while(l<=r){
+    //       if(A[l]<=A[r]){
+    //           maxx=max(maxx,r-l);
+    //           l++;
+    //           r=N-1;
+    //       }       
+    //       else{
+    //           r--;
+    //       }
+    //   }
+    //   return maxx;
+    // }
+    // int maxIndexDiff(int A[], int N) 
+    //   { 
+    //       int ans=0;
+    //       stack<int> st;
+    //       int i;
+    //       for(i=0;i<N;i++)
+    //       {
+    //           if(st.empty() || A[st.top()]>A[i])
+    //           {
+    //               st.push(i);
+    //           }
+    //       }
+    //       for(i=N-1;i>=0;i--)
+    //       {
+    //           while(!st.empty() && A[i]>=A[st.top()])
+    //           {
+    //               ans=max(ans,i-st.top());
+    //               st.pop();
+    //           }
+    //       }
+    //       return ans;
+    //       // Your code here
+    //   }
+    int maxIndexDiff(int arr[], int N) 
     { 
-        // Your code here
-        int l=0,r=N-1,maxx=-1;
-       while(l<=r){
-           if(A[l]<=A[r]){
-               maxx=max(maxx,r-l);
-               l++;
-               r=N-1;
-           }       
-           else{
-               r--;
-           }
-       }
-       return maxx;
+        vector<int> lmin(N),rmax(N);
+        lmin[0] = arr[0];  // storing all the minimum values from 0
+        for(int i=1;i<N;i++){
+            lmin[i] = min(arr[i],lmin[i-1]);
+        }
+        rmax[N-1] = arr[N-1]; // storing all the maximum values from N-1
+        for(int i=N-2;i>=0;i--){
+            rmax[i] = max(arr[i],rmax[i+1]);
+        }
+        
+        int i=0,j=0,ans=-1;
+        while(i<N and j<N){
+            if(lmin[i]<=rmax[j]){
+                ans = max(ans,j-i);
+                j++;
+            }
+            else i++;
+        }
+        return ans;
     }
+    
 };
 
 // { Driver Code Starts.
