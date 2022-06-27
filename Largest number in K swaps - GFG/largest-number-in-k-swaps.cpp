e6bcd352ecb953028825f1a6a6ba89ede5333834
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-27 19:18:55 +0530

    Added solution - LeetHub

--- Largest number in K swaps - GFG/largest-number-in-k-swaps.cpp
+++ Largest number in K swaps - GFG/largest-number-in-k-swaps.cpp
@@ -8,44 +8,28 @@ using namespace std;
 
 class Solution
 {
-   public:
-   //Function to find the largest number after k swaps.
-   
-   void backtrack(string & temp, string &s , int idx, int k){
-       if(idx== s.size() or k==0) return; //BASE CASE
-       int ii = idx;
-       char maxv = temp[idx];
-       // Finding the maximum character in the remaining substring
-       for(int i=idx+1;i<s.size();i++){
-           if(temp[i]>maxv){
-               maxv = temp[i];
-               ii = i;
-           }
-       }
-       // if s[idx] is the maximum character
-       if(ii==idx){
-           backtrack(temp,s,idx+1,k);
-       }
-       else{
-           vector<int> indices; //To store all possible indices of highest value
-           for(int i=idx+1;i<s.size();i++){
-               if(temp[i]==maxv) indices.push_back(i);
-           }
-           for(auto it : indices){
-               swap(temp[it],temp[idx]);
-               backtrack(temp,s,idx+1,k-1);
-               s = max(s,temp);
-               swap(temp[it],temp[idx]); //bakctrack
-           }
-       }
-   }
-   
-   string findMaximumNum(string s, int k)
-   {
-      string temp = s;
-      backtrack(temp,s,0,k);
-      return s;
-   }
+    public:
+    void rec(string str, int k, int a, string &ans){
+        ans=max(ans,str);
+        if(k==0) return;
+        for(int i=a;i<str.length()-1;i++){
+            for(int j=i+1;j<str.length();j++){
+                if(str[j]>str[i]){
+                    swap(str[i],str[j]);
+                    rec(str,k-1,i+1,ans);
+                    swap(str[i],str[j]);
+                }
+            }
+        }
+    }
+    
+    string findMaximumNum(string str, int k)
+    {
+       // code here.
+       string ans=str;
+       rec(str,k,0,ans);
+       return ans;
+    }
 };
 
 // { Driver Code Starts.
