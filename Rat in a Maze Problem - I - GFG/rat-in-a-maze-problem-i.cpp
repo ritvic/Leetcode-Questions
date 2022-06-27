e36f38c19b86e41be6a0e84ea9d597f26dc8cc78
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-27 18:50:48 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -11,28 +11,27 @@ using namespace std;
 class Solution{
     public:
      vector<string> ans;
-    void solve ( int i, int j , string s , int n, vector<vector<int>> m, vector<vector<int>> & vis){
+    void solve ( int i, int j , string s , int n, vector<vector<int>> m){
         if (i<0 or j< 0 or i>=n or j>= n) return;
-        if (vis[i][j] ==1 or m[i][j] == 0) return ;
+        if (m[i][j] != 1) return ;
         
         if (i == n-1 and j == n-1){
             ans.push_back(s);
             return ;
         }
-        vis[i][j] =1;
-        solve (i-1, j , s+"U" , n , m, vis);
-        solve (i+1, j, s+"D", n, m, vis);
-        solve (i, j-1, s+"L", n, m, vis);
-        solve ( i, j+1, s+"R", n, m, vis);
-        vis[i][j] =0;
+        m[i][j] =2;
+        solve (i-1, j , s+"U" , n , m);
+        solve (i+1, j, s+"D", n, m);
+        solve (i, j-1, s+"L", n, m);
+        solve ( i, j+1, s+"R", n, m);
+        m[i][j] =1;
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         if (m[0][0]==0 )return ans;
-        vector<vector<int>> vis( n, vector<int>( n));
         if (m[n-1][n-1] == 0) return ans;
         string s="";
-        solve ( 0,0, s, n , m ,vis );
+        solve ( 0,0, s, n , m);
         sort ( ans.begin(),ans.end());
         return ans;
         
