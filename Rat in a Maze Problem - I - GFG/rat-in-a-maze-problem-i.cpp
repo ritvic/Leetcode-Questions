da79d4aa5f3d2c78cb758a0e74dce6614eafe6eb
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-27 19:15:38 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -8,33 +8,31 @@ using namespace std;
  // } Driver Code Ends
 // User function template for C++
 
+int dir[4] = {1,-1,0,0};
+string D="DULR";
 class Solution{
+    vector<string> ans;
     public:
-     vector<string> ans;
-    void solve ( int i, int j , string s , int n, vector<vector<int>> &m){
-        if (i<0 or j< 0 or i>=n or j>= n) return;
-        if (m[i][j] != 1) return ;
-        
-        if (i == n-1 and j == n-1){
-            ans.push_back(s);
-            return ;
+    void dfs(int i,int j,int n,string route,vector<vector<int>> &m){
+        if(!m[i][j])
+            return;
+        if(i==n-1 && j==n-1){
+            ans.push_back(route);
+            return;
         }
-        m[i][j] =2;
-        solve (i-1, j , s+"U" , n , m);
-        solve (i+1, j, s+"D", n, m);
-        solve (i, j-1, s+"L", n, m);
-        solve ( i, j+1, s+"R", n, m);
-        m[i][j] =1;
+        m[i][j]=0;
+        for(int k=0;k<4;k++){
+            int x= i+dir[k], y =j+dir[3-k];
+            if(x>=0 && y>=0 && x<n && y<n && m[x][y])
+                dfs(x,y,n,route+D[k],m);
+        }
+        m[i][j]=1;
     }
+    
     vector<string> findPath(vector<vector<int>> &m, int n) {
-        // Your code goes here
-        if (m[0][0]==0 )return ans;
-        if (m[n-1][n-1] == 0) return ans;
-        string s="";
-        solve ( 0,0, s, n , m);
-        sort ( ans.begin(),ans.end());
+        dfs(0,0,n,"",m);
+        sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
 
