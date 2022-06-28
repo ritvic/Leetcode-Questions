4d2f0a385eaf7946a843c6de55bb95841594b1e0
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-28 15:13:44 +0530

    Added solution - LeetHub

--- Solve the Sudoku - GFG/solve-the-sudoku.cpp
+++ Solve the Sudoku - GFG/solve-the-sudoku.cpp
@@ -14,12 +14,18 @@ using namespace std;
 class Solution {
 public:
     //Function to find a solved Sudoku. 
-    bool SolveSudoku(int grid[N][N]) { 
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(grid[i][j] == 0) {
-                    for(char c=1; c<=9; c++) {
-                        if(isValid(grid, i, j, c)) {
+    bool SolveSudoku(int grid[N][N]){ 
+        
+        for(int i=0; i<N; i++) 
+        {
+            for(int j=0; j<N; j++) 
+            {
+                if(grid[i][j] == 0) 
+                {
+                    for(int c=1; c<=9; c++) 
+                    {
+                        if(isValid(grid, i, j, c)) 
+                        {
                             grid[i][j] = c;
                             if(SolveSudoku(grid) == true) return true;
                             else grid[i][j] = 0;
@@ -32,8 +38,9 @@ public:
         return true;
     }
     
-    bool isValid(int grid[N][N], int row, int col, char c) {
-        for(int i=0; i<9; i++) {
+    bool isValid(int grid[N][N], int row, int col, int c) {
+        for(int i=0; i<9; i++) 
+        {
             if(grid[i][col] == c) return false;
             if(grid[row][i] == c) return false;
             if(grid[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c) return false;
@@ -43,11 +50,10 @@ public:
     
     //Function to print grids of the Sudoku.
     void printGrid (int grid[N][N]) {
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
+        
+        for(int i=0; i<N; i++) 
+            for(int j=0; j<N; j++) 
                 std::cout<< grid[i][j] << " ";
-            }
-        }
     }
 };
 
