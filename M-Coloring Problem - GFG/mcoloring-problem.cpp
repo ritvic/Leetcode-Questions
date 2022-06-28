e6c510d64fd5fbb3a9b39dde3f88e771635b63d7
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-28 13:51:46 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -6,28 +6,52 @@ using namespace std;
  // } Driver Code Ends
 // Function to determine if graph can be coloured with at most M colours such
 // that no two adjacent vertices of graph are coloured with same colour.
-    bool isSafe(bool graph[101][101],int node,int c,vector<int>&color){
-        for(int i=0;i<color.size();i++){
-            if(graph[node][i]&&color[i]==c)return false;
-        }
-        return true;
+    bool isSafe(int v, bool graph[101][101], int color[101], int c, int V) {
+    // checking if the connected nodes to v have same colour as c.
+    for (int i = 0; i < V; i++)
+        if (graph[v][i] && c == color[i]) return false;
+
+    // returning true if no connected node has same colour.
+    return true;
     }
-    bool solve(bool graph[101][101],vector<int>&color,int m,int n,int node){
-        if(node ==n){
-            return true;
-        }
-        for(int c=1;c<=m;c++){
-            if(isSafe(graph,node,c,color)){
-                color[node]=c;
-                if(solve(graph,color,m,n,node+1))return true;
-                color[node]=0;
+    
+    bool graphColoringUtil(bool graph[101][101], int m, int color[101], int v,
+                           int V) {
+        // if all vertices have been assigned colour then we return true.
+        if (v == V) return true;
+    
+        for (int c = 1; c <= m; c++) {
+            // checking if this colour can be given to a particular node.
+            if (isSafe(v, graph, color, c, V)) {
+                // assigning colour to the node.
+                color[v] = c;
+    
+                // calling function recursively and checking if other nodes
+                // can be assigned other colours.
+                if (graphColoringUtil(graph, m, color, v + 1, V) == true)
+                    // returning true if the current allocation was successful.
+                    return true;
+    
+                // if not true, we backtrack and remove the colour for that node.
+                color[v] = 0;
             }
         }
+        // if no colour can be assigned, we return false.
         return false;
     }
+    
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
     bool graphColoring(bool graph[101][101], int m, int n) {
-    vector<int>color(n);
-    return solve(graph,color,m,n,0);
+        int *color = new int[n];
+        for (int i = 0; i < n; i++) color[i] = 0;
+    
+        // checking if colours can be assigned.
+        if (graphColoringUtil(graph, m, color, 0, n) == false) {
+            return false;
+        }
+    
+        return true;
     }
 
 
