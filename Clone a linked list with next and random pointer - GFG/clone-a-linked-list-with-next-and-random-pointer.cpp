908c073a9eb1a36303055e68f7b279dc458d900a
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-06-29 12:44:26 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
@@ -21,28 +21,55 @@ struct Node {
 class Solution
 {
     public:
-    Node *copyList(Node *head)
+    Node *copyList(Node *start)
     {
         //Write your code here
-        unordered_map<Node*,Node*> m;
-        Node* copy=new Node(head->data),*c=copy,*temp=head->next;
-        m[head]=copy;
-        while(temp)
-        {
-            Node* t=new Node(temp->data);
-            c->next=t;
-            c=c->next;
-            m[temp]=c;
-            temp=temp->next;
-        }
-        temp=head,c=copy;
-        while(temp)
-        {
-            c->arb=m[temp->arb];
-            temp=temp->next;
-            c=c->next;
-        }
-        return copy;
+        Node* curr = start, *temp;
+  
+    // insert additional node after
+    // every node of original list
+    while (curr)
+    {
+        temp = curr->next;
+  
+        // Inserting node
+        curr->next = new Node(curr->data);
+        curr->next->next = temp;
+        curr = temp;
+    }
+  
+    curr = start;
+  
+    // adjust the random pointers of the
+    // newly added nodes
+    while (curr)
+    {
+        if(curr->next)
+            curr->next->arb = curr->arb ? 
+                                 curr->arb->next : curr->arb;
+  
+        // move to the next newly added node by
+        // skipping an original node
+        curr = curr->next?curr->next->next:curr->next;
+    }
+  
+    Node* original = start, *copy = start->next;
+  
+    // save the start of copied linked list
+    temp = copy;
+  
+    // now separate the original list and copied list
+    while (original && copy)
+    {
+        original->next =
+         original->next? original->next->next : original->next;
+  
+        copy->next = copy->next?copy->next->next:copy->next;
+        original = original->next;
+        copy = copy->next;
+    }
+  
+    return temp;
     }
 
 };
