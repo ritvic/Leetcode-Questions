683b06e2d7fa540eda3768abdbfc7c561e8d37a5
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-01 17:24:47 +0530

    Added solution - LeetHub

---
+++ Sum of two numbers without using arithmetic operators - GFG/sum-of-two-numbers-without-using-arithmetic-operators.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    int sum(int a , int b)
+    {
+        //code here
+         int add=a^b;
+       int carry=a&b;
+       if(carry==0)
+       return add;
+       else
+       return sum(add,carry<<1);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b;
+        cin >> a>>b;
+        
+        Solution ob;
+        cout<< ob.sum(a,b) <<"\n";
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
