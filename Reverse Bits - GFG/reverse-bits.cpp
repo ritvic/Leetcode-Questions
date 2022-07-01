55ecd1528e97db9dcf86a54cd49ad6f3ed84476b
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-01 17:47:34 +0530

    Added solution - LeetHub

--- Reverse Bits - GFG/reverse-bits.cpp
+++ Reverse Bits - GFG/reverse-bits.cpp
@@ -5,16 +5,21 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    long long reversedBits(long long X) {
-       // code here
-       long long res = 0;
-       for(int i = 0; i < 32; i++){
-           res <<= 1;
-           if(X & 1) res++;
-           X >>= 1;
-       }
-       return res;
-   }
+//     long long reversedBits(long long X) {
+//       // code here
+//       long long res = 0;
+//       for(int i = 0; i < 32; i++){
+//           res <<= 1;
+//           if(X & 1) res++;
+//           X >>= 1;
+//       }
+//       return res;
+//   }
+   long long reversedBits(long long x) {
+        string str = bitset<32>(x).to_string();
+        reverse(str.begin(),str.end());
+        return stoll(str, 0, 2);
+    }
 };
 
 // { Driver Code Starts.
