475d52d6cda597ec282a1a6534920cc36dc66bcd
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-06 15:56:40 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -9,20 +9,38 @@ using namespace std;
 class Solution{
 public:	
 	// calculate the maximum sum with out adjacent
-	int f(int index,int*arr,vector<int>&dp)
-	{
-	    if(index==0)return arr[index];
-	    if(index<0)return 0;
-	    if(dp[index]!=-1)return dp[index];
-	    int take= arr[index]+f(index-2,arr,dp);
-	    int not_take=0+f(index-1,arr,dp);
-	    return dp[index]=max(take,not_take);
-	}
+// 	int f(int index,int*arr,vector<int>&dp)
+// 	{
+// 	    if(index==0)return arr[index];
+// 	    if(index<0)return 0;
+// 	    if(dp[index]!=-1)return dp[index];
+// 	    int take= arr[index]+f(index-2,arr,dp);
+// 	    int not_take=0+f(index-1,arr,dp);
+// 	    return dp[index]=max(take,not_take);
+// 	}
+// 	int findMaxSum(int *arr, int n)
+// 	{
+// 	    vector<int>dp(n,-1);
+// 	    return f(n-1,arr,dp);
+// 	}
 	int findMaxSum(int *arr, int n)
-	{
-	    vector<int>dp(n,-1);
-	    return f(n-1,arr,dp);
-	}
+    {
+      int prev=arr[0];
+       int prev2=0;
+       int curr;
+       
+       for(int i=1;i<n;i++)
+       {
+           int take=arr[i];
+        //   if(i>1)
+           take+=prev2;
+           int not_take=prev;
+           curr=max(take,not_take);
+           prev2=prev;
+           prev=curr;
+       }
+       return prev;
+    }
 };
 
 // { Driver Code Starts.
