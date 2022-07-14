5256938626da10c3e61be9cc5f71f1dd967cc031
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-14 17:50:19 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &v, int m, int n){
+        // code here       
+        int k=(n*m+1)/2,ans;
+        sort(v.begin(),v.end());
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
+        for (int r = 0; r < min(m, k); ++r)
+            minHeap.push({v[r][0], r, 0});
+
+        for (int i = 1; i <= k; ++i) {
+            auto top = minHeap.top(); minHeap.pop();
+            int r = top[1], c = top[2];
+            ans = top[0];
+            if (c + 1 < n) minHeap.push({v[r][c + 1], r, c + 1});
+        }
+        return ans;
+    }
+    
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}  // } Driver Code Ends
