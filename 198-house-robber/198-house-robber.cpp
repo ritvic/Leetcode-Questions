6bda3eae9ba56c5c429409a8d96232b9073eb414
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-27 00:37:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums) { 
-        int n = nums.size(), pre = 0, cur = 0;
+        int n = nums.size(), pre = 0, cur = 0,temp;
         for (int i = 0; i < n; i++) {
-            int temp = max(pre + nums[i], cur);
+            temp = max(pre + nums[i], cur);
             pre = cur;
             cur = temp;
         }
