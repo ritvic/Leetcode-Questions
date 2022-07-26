1ea5e257a3b1584f064423ada1b524c3237553f6
Ritvik Aggarwal <59195045+ritvic@users.noreply.github.com>
2022-07-27 00:09:46 +0530

    Time: 252 ms (52.69%), Space: 85 MB (86.18%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> s; //First is the number while second is the position
+        
+        int n= temperatures.size();
+        vector<int> result(n,0);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            int curr = temperatures[i];
+            while(!s.empty() && s.top().first <= curr) s.pop();
+            
+            result[i] = s.empty() ? 0 : s.top().second - i;
+
+            s.push({curr,i});
+        }
+        
+        return result;
+    }
+};
